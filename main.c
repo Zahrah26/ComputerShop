2667949f3f3d8943dd336f27389bf90fa882faee
Shreya Doorgachurn <shreya.doorgachurn1@umail.uom.ac.mu>
2024-09-26 12:13:56 +0400

    Modify main

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
-# include <Stdio.h>
+#include <stdio.h>
+#include "product_management.h"
+#include "user_management.h"
 
-int main(){
+int main() {
+    int choice;
+    printf("Welcome:\n");
 
+    while (1) {
+        printf("1. Manage Product (staff)\n");
+        printf("2. Manage User\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-
-
+        switch (choice) {
+            case 1:
+                productStaffMenu();
+                break;
+            case 2:
+                staffMenu();
+            case 3:
+                break;
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    }
     return 0;
 }
