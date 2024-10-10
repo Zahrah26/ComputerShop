da10f3043dac7b8fcf57a6ca5c18b55bd9a53a00
Oushvina <oushvina.seewoobudut@umail.uom.ac.mu>
2024-10-10 21:04:29 +0400

    Modified

--- ComputerShopmenu.c
+++ ComputerShopmenu.c
@@ -24,34 +24,32 @@ void display_main_menu() {
     printf("Enter your choice: ");
 }
 
-void clear_screen() {
-    system("cls");  // For Windows, this clears the console screen
-}
+
 
 int main() {
     int choice;
     int running = 1;
 
     while (running) {
-        clear_screen();  // Clear the screen before displaying the menu
+        
         display_main_menu();
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                clear_screen();  // Clear the screen before going to the next menu
+                
                 productStaffMenu();
                 break;
             case 2:
-                clear_screen();
+                
                 UserMenu();
                 break;
             case 3:
-                clear_screen();
+                
                 payment_processing_system();
                 break;
             case 4:
-                clear_screen();
+                
                 after_sales_service_system();
                 break;
             case 5:
